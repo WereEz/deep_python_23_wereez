ddaddf8c6ec65de7206ab1cce1ae03aaa0e38271
Denis <denis03122002@mail.ru>
2023-10-19 16:16:15 +0700

    Дз4 попытка 2

--- 04/test_descriptors.py
+++ 04/test_descriptors.py
@@ -27,9 +27,24 @@ def test_string_descriptor(test_data):
 
 def test_positive_integer_descriptor(test_data):
     assert test_data.price == 100
-    test_data.num = 0
-    assert test_data.num == 0
+    test_data.price = 0
+    assert test_data.price == 0
     with pytest.raises(ValueError):
         test_data.price = 'string'
     with pytest.raises(ValueError):
         test_data.price = -50
+
+
+def test_invalid_value(test_data):
+    assert test_data.num == 10
+    with pytest.raises(ValueError):
+        test_data.num = "asidja"
+    assert test_data.num == 10
+    assert test_data.name == 'example'
+    with pytest.raises(ValueError):
+        test_data.name = 123
+    assert test_data.name == "example"
+    assert test_data.price == 100
+    with pytest.raises(ValueError):
+        test_data.price = -1
+    assert test_data.price == 100
