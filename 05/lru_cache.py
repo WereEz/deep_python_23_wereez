644254861b029fc6de7c7b51f54c5cb20c799c4a
Denis <denis03122002@mail.ru>
2023-10-24 16:16:26 +0700

    Дз5 попытка 1

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -19,19 +19,3 @@ class LRUCache:
         if self.limit <= len(self.dct):
             self.dct.pop(next(iter(self.dct)))
         self.dct[key] = value
-
-
-cache = LRUCache(2)
-
-cache.set("k1", "val1")
-cache.set("k2", "val2")
-
-assert cache.get("k3") is None
-assert cache.get("k2") == "val2"
-assert cache.get("k1") == "val1"
-
-cache.set("k3", "val3")
-
-assert cache.get("k3") == "val3"
-assert cache.get("k2") is None
-assert cache.get("k1") == "val1"
