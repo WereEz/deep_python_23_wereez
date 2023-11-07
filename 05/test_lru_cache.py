6668a2281d0618d5b7962079c701879d3d06a671
Denis <denis03122002@mail.ru>
2023-11-07 14:38:36 +0700

    Дз5 попытка 2

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -1,6 +1,17 @@
 import pytest
 from lru_cache import LRUCache
 
+def test_as_in_task():
+    cache = LRUCache(2)
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+    assert cache.get("k3") is None
+    assert cache.get("k2") == "val2"
+    assert cache.get("k1") == "val1"
+    cache.set("k3", "val3")
+    assert cache.get("k3") == "val3"
+    assert cache.get("k2") is None
+    assert cache.get("k1") == "val1"
 
 def test_incorrect_limit():
     with pytest.raises(ValueError):
